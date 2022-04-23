727fb531b81f62a1e7b829c3853dcd9a17c68162
Rachel Gray <rachel.gray7448@gmail.com>
2022-04-23 13:16:42 -0400

    set up server.js for routing

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/html');
+const fs = require('fs');
+const PORT = process.env.PORT || 3001;
 const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+const { notes } = require('./db/db.json');
 
-//middleware
-// port
-// app.use for api and html routes
-// app.listen
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
+
+
